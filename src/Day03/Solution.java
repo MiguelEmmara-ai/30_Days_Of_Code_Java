30b3da9297a1e3bda1893814d4642425b23c79f7
Miguel Emmara <m.miguelemmara@gmail.com>
2021-04-24 17:59:28 +1200

    Day 3

--- src/Day03/Solution.java
+++ src/Day03/Solution.java
@@ -6,27 +6,21 @@ import java.util.Scanner;
  * Created by Miguel Emmara
  */
 public class Solution {
-    // Complete the solve function below.
-    static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        double tip = (meal_cost / 100) * tip_percent;
-        double tax = (meal_cost * tax_percent) / 100;
-        double total_cost = meal_cost + tip + tax;
-        System.out.println(Math.round(total_cost));
-    }
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double meal_cost = scanner.nextDouble();
+        int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int tip_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tax_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        solve(meal_cost, tip_percent, tax_percent);
+        if (N % 2 != 0) {
+            System.out.println("Weird");
+        } else if (N >= 2 && N <= 5) {
+            System.out.println("Not Weird");
+        } else if (N % 2 == 0 && N >= 6 && N <= 20) {
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && N > 20) {
+            System.out.println("Not Weird");
+        }
 
         scanner.close();
     }
---
+++ src/Day04/Solution.java
@@ -0,0 +1,10 @@
+package Day04;
+
+/**
+ * Created by Miguel Emmara
+ */
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+}
